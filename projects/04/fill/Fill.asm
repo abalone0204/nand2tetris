e2c09313542286e9dd2d940629c42a8a0563cab2
abalone0204 <abalone0204@gmail.com>
2015-08-05 01:22:28 +0800

    done fil

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,73 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+//(Listen)
+//if KBD != 0
+//    i = 0 
+//    for i < SCREEN.length {
+//        SCREEN[i] = -1
+//        i = i + 1
+//    }
+//else
+//    for i < SCREEN.length {
+//        SCREEN[i] = 0
+//        i = i + 1
+//    }
+//goto Listen
+
+(Listen)
+@SCREEN
+D=A
+@addr
+M=D
+@i
+M=0
+
+@KBD
+D=M
+@Bright
+D; JEQ
+(Dark)
+@addr
+D=M
+@i
+A=D+M
+M=-1
+@i
+M=M+1
+
+@addr
+D=M
+@i
+D=D+M
+@KBD
+D=A-D
+@End
+D;JEQ
+@Dark
+D;JNE
+
+(Bright)
+@addr
+D=M
+@i
+A=D+M
+M=0
+@i
+M=M+1
+
+@addr
+D=M
+@i
+D=D+M
+@KBD
+D=A-D
+@End
+D;JEQ
+@Bright
+D;JNE
+
+(End)
+@Listen
+0;JMP
