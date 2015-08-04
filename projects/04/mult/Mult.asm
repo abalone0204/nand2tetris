2db45e5f08dc03ac05cd78c1b7dad6b91fc05b41
abalone0204 <abalone0204@gmail.com>
2015-08-04 17:07:13 +0800

    done multiply

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,61 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+//
+// m=r0
+// n=r1
+// i=0
+// result=0
+// for i< n {
+//     result = result+m
+//     i= i+1
+// }
+// r2 = result
+
+@R0
+D=M
+@m
+M=D
+// if m == 0 then end
+@End
+D;JEQ
+
+@R1
+D=M
+@n
+M=D
+D=M
+// if n == 0 then end
+@End
+D;JEQ
+
+@i
+M=0
+@result
+M=0
+
+// init all vars done
+(Loop)
+@result
+D=M
+@m
+D=D+M
+
+@result
+M=D
+
+@i
+M=M+1
+D=M
+@n
+D=M-D
+@Loop
+D; JGT
+
+@result
+D=M
+@R2
+M=D
+(End)
+@End
+0;JMP
